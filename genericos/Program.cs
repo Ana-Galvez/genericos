9840bc686fe6eaa0064ba249c36d054236737871
anamarigalvez1688@gmail.com <amg1688>
2025-02-26 00:25:33 -0300

    Agregar archivos de proyecto.

---
+++ genericos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace genericos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    //SIN GENERICS, USANDO HERENCIA
+    class Objetos
+    {
+        private Object[] lista;
+        private int cantidadElementos=0;
+        public Objetos(int cantidadElementos)
+        {
+            lista = new Object[cantidadElementos];
+        }
+        public void Agregar(Object elemento)
+        {
+            lista[cantidadElementos] = elemento;
+            cantidadElementos++;
+        }
+
+        public Object GetLista(int cantidadElementos)
+        {
+            return lista[cantidadElementos];
+        }
+
+    }
+}
