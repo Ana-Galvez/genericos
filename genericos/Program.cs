49e008e5a022edc113a93fc0217cc7a9d42fba25
anamarigalvez1688@gmail.com <amg1688>
2025-02-26 01:20:02 -0300

    feat: practica con generics 1

--- genericos/Program.cs
+++ genericos/Program.cs
@@ -8,47 +8,76 @@ namespace genericos
     {
         static void Main(string[] args)
         {
-            Lista nombres = new Lista(4);
-            //nombres.Agregar(100);
-            //nombres.Agregar("Juan");
-            //nombres.Agregar(125);
-            //nombres.Agregar("Marco");
-            ////se debe castear
-            //int nombre = (int)nombres.GetLista(2);
-            //Console.WriteLine(nombre.GetType());
-            nombres.Agregar(new Empleado(20000));
-            nombres.Agregar(new Empleado(20000));
-            nombres.Agregar(new Empleado(20000));
-            nombres.Agregar(new Empleado(20000));
+            //Lista nombres = new Lista(4);
+            ////nombres.Agregar(100);
+            ////nombres.Agregar("Juan");
+            ////nombres.Agregar(125);
+            ////nombres.Agregar("Marco");
+            //////se debe castear
+            ////int nombre = (int)nombres.GetLista(2);
+            ////Console.WriteLine(nombre.GetType());
+            //nombres.Agregar(new Empleado(20000));
+            //nombres.Agregar(new Empleado(20000));
+            //nombres.Agregar(new Empleado(20000));
+            //nombres.Agregar(new Empleado(20000));
 
-            //no tira error de tipo de dato hasta que se ejecuta
-            String nombre = (String)nombres.GetLista(2);
+            ////no tira error de tipo de dato hasta que se ejecuta
+            //String nombre = (String)nombres.GetLista(2);
+
+            Lista<Empleado> salarios = new Lista<Empleado>(4);
+            salarios.Agregar(new Empleado(20000));
+            salarios.Agregar(new Empleado(25000));
+            salarios.Agregar(new Empleado(2000));
+            salarios.Agregar(new Empleado(10000));
+            Empleado salario = salarios.GetLista(2);
+            Console.WriteLine(salario.GetSalario()+4);
+
 
 
         }
     }
 
-    //SIN GENERICS, USANDO HERENCIA
-    class Lista
+    //Con GENERICS
+    class Lista<T>
     {
-        private Object[] lista;
-        private int cantidadElementos=0;
+        private T[] lista;
+        private int cantidadElementos = 0;
         public Lista(int cantidadElementos)
         {
-            lista = new Object[cantidadElementos];
+            lista = new T[cantidadElementos];
         }
-        public void Agregar(Object elemento)
+        public void Agregar(T elemento)
         {
             lista[cantidadElementos] = elemento;
             cantidadElementos++;
         }
-
-        public Object GetLista(int indiceElemento)
+        public T GetLista(int indiceElemento)
         {
             return lista[indiceElemento];
         }
-
     }
+
+    ////SIN GENERICS, USANDO HERENCIA
+    //class Lista
+    //{
+    //    private Object[] lista;
+    //    private int cantidadElementos=0;
+    //    public Lista(int cantidadElementos)
+    //    {
+    //        lista = new Object[cantidadElementos];
+    //    }
+    //    public void Agregar(Object elemento)
+    //    {
+    //        lista[cantidadElementos] = elemento;
+    //        cantidadElementos++;
+    //    }
+
+    //    public Object GetLista(int indiceElemento)
+    //    {
+    //        return lista[indiceElemento];
+    //    }
+
+    //}
 
     class Empleado
     {
